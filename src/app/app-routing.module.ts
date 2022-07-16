fc018f02866894b8648edf1e82887dc79d124cc1
IQBOLJON <iqboljon.ismoilov.98mail.ru>
2022-07-16 11:32:53 +0500

    added files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'
+import {HomeComponent} from "./home/home.component";
+import {BookManagerComponent} from "./book-manager/book-manager.component";
+import {ContactComponent} from "./contact/contact.component";
+import {TaskManagerComponent} from "./task-manager/task-manager.component";
+import {NotFoundError} from "rxjs";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'book', component: BookManagerComponent},
+  {path: 'task', component: TaskManagerComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: NotFoundComponent}
+]
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookManagerComponent } from './book-manager/book-manager.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookManagerComponent,
+    TaskManagerComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
